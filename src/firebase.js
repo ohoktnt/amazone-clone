330266ebd4dd13dc7ae060e6cac542a50351cad7
Tammy <tammy.n.tran@gmail.com>
2021-06-28 11:49:21 -0400

    clean up app, add firebase config.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrmOtrBmi6-3GRgfZijCEuAK6jYJalogE",
+  authDomain: "clone-87b0d.firebaseapp.com",
+  projectId: "clone-87b0d",
+  storageBucket: "clone-87b0d.appspot.com",
+  messagingSenderId: "939908426444",
+  appId: "1:939908426444:web:70c90740b23ca085a1b069",
+  measurementId: "G-NELM99WPQ6"
+};
