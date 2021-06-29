b65d99c5a5ba5cf5d55ec3a1d9c6e678a0773f4a
Tammy <tammy.n.tran@gmail.com>
2021-06-29 12:12:08 -0400

    created login component and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,12 +10,16 @@ function App() {
     // BEM convention for CSS
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
