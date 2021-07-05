2b86873ed443e5f975814741defcc16f8adab05f
Tammy <tammy.n.tran@gmail.com>
2021-07-05 14:59:29 -0400

    created payment page and styled

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }] = useStateValue();
 
   //// Better solution/practice is to make it a selector in the reducer file
@@ -35,7 +37,9 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
